90c60ddbd9222572cfa40db7c8b5f36ec909e2b2
Tomáš Kryčfalušij <91247418+TomasKrycfalusij@users.noreply.github.com>
2022-04-09 05:49:23 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-x = 2
-y = 0
+a = 2
+b = 0
 displej = 5
 
 for i in range(displej):
     led.set_brightness(100)
-    led.plot(x, y)
-    y += 1
+    led.plot(a, b)
+    b += 1
+
+x = 0
+led.plot(x ,y)
+y = randint(0, 4)
--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
-let x = 2
-let y = 0
+let a = 2
+let b = 0
 let displej = 5
 for (let i = 0; i < displej; i++) {
     led.setBrightness(100)
-    led.plot(x, y)
-    y += 1
+    led.plot(a, b)
+    b += 1
 }
+let x = 0
+led.plot(x, y)
+let y = randint(0, 4)
